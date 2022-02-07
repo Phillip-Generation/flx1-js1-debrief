b9e1e4ffc9f76e9763c5fe66bc522477cc0cb727
flx1derrick <95151579+flx1derrick@users.noreply.github.com>
2022-02-07 17:37:16 -0600

    add comments to genbuzz

--- js/js-1-debrief.js
+++ js/js-1-debrief.js
@@ -5,25 +5,26 @@
 const myNumber = 10;
 let modThree = false;
 let modFive = false;
-
+// Check myNumber is type Number
 if (typeof myNumber !== 'number') {
   console.log('This is not a number');
 }
-
+// Check myNumber factor 3
 else if (typeof myNumber == 'number' && myNumber % 3 == 0) {
   console.log('Gen');
   modThree = true;
 }
+// Check myNumber factor 5
 else if (typeof myNumber == 'number' && myNumber % 5 == 0) {
   console.log('Buzz');
   modFive = true;
 }
-
+// Check myNumber factors of 3 and 5 
 else if (typeof myNumber == 'number' && modThree && modFive) {
   console.log('GenBuzz');
   modThree = true;
   modFive = true;
 }
-
+// Print myNumber if not either factor 3 or 5 
 else
   console.log(myNumber);
