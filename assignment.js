0d7877ef6992ebd1f23c4dbae0f822e78a341e03
Sripoorna <sripuranapanda@gmail.com>
2022-02-08 11:20:51 -0800

    completed tasks

--- assignment.js
+++ assignment.js
@@ -1,3 +1,5 @@
+/* 1. GenBuzz */
+
 let myNumber= 15;
 
 if(typeof myNumber!== Number){
@@ -18,4 +20,41 @@ else if(myNumber%3 ==0){
 }
 else{
     console.log(myNumber);
+}
+
+
+/* E-Commerce Item List */
+
+let item = "Shoes";
+let itemPrice = "";
+
+switch(item){
+ case 'Shoes':
+     itemPrice = "Shoes are $50";
+     break;
+
+ case 'Jeans':
+     itemPrice = "Jeans are $25";
+     break;
+
+ case 'Hat':
+     itemPrice = "Hat is $12";
+     break;
+
+ case 'Socks':
+    itemPrice = "Socks are $12";
+    break;
+
+ default:
+     itemPrice = "Invalid item";
+     break;
+}
+console.log(itemPrice);
+
+/* Printing random integer between 50 and 100 */
+
+let randomNumber = Math.floor(Math.random()*100);
+
+if(randomNumber> 50){
+ console.log(randomNumber);
 }
